ff507245dbcab81a751bb1181aab7362d8aa9a16
EvgeniJD <evgeni.y.dimitrov1985@gmail.com>
2020-12-22 20:40:20 +0200

    timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timer';
+
+  endDate = new Date('Dec 22 2020 20:39:00');
+  timeTriggers = [new Date('Dec 22 2020 20:38:30'), new Date('Dec 22 2020 20:38:35')];
+
+  logInfo(text) {
+    console.log(text);
+  }
 }
